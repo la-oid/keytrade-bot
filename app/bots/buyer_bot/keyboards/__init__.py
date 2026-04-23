c9509fd7b651463554a6e3a183aaab32f0e93758
Lev <me@la-oid.dev>
2026-04-23 13:32:31 +0300

    Update __init__.py

--- app/bots/buyer_bot/keyboards/__init__.py
+++ app/bots/buyer_bot/keyboards/__init__.py
@@ -1,10 +1,11 @@
-from .inline import MenuKeyboards, MiscKeyboards
+from .inline import MenuKeyboards, MiscKeyboards, WholesaleKeyboards
 
 
 class InlineKeyboards:
     def __init__(self):
         self.menu = MenuKeyboards()
         self.misc = MiscKeyboards()
+        self.wholesale = WholesaleKeyboards()
 
 
 __all__ = ["InlineKeyboards"]
