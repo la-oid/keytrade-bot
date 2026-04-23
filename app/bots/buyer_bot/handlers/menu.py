6443ab8465b33a5bfb87ffc75991816198c0e221
Lev <me@la-oid.dev>
2026-04-23 13:36:01 +0300

    Update menu.py

--- app/bots/buyer_bot/handlers/menu.py
+++ app/bots/buyer_bot/handlers/menu.py
@@ -15,20 +15,27 @@ buttons = InlineKeyboards()
 async def profile_handler(call: CallbackQuery, user):
     await call.answer()
     await call.message.edit_text(
-        texts.menu.PROFILE_TEXT.format(user_id=user.telegram_id, orders="Заказов пока нет")
+        texts.menu.PROFILE_TEXT.format(user_id=user.telegram_id, orders="Заказов пока нет"),
+        reply_markup=buttons.menu.profile
     )
 
 
 @r.callback_query(F.data == "about")
 async def about_handler(call: CallbackQuery):
     await call.answer()
-    await call.message.edit_text(texts.menu.ABOUT_TEXT)
+    await call.message.edit_text(
+        texts.menu.ABOUT_TEXT,
+        reply_markup=buttons.menu.back_to_menu
+    )
 
 
 @r.callback_query(F.data == "support")
 async def support_handler(call: CallbackQuery):
     await call.answer()
-    await call.message.edit_text(texts.menu.SUPPORT_TEXT)
+    await call.message.edit_text(
+        texts.menu.SUPPORT_TEXT,
+        reply_markup=buttons.menu.back_to_menu
+    )
 
 
 @r.callback_query(F.data == "back_to_menu")
