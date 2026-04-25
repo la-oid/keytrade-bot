f9f18dc028141c158b79e3fcafd0ed3a99384e45
Lev <me@la-oid.dev>
2026-04-25 14:57:45 +0300

    Update payment.py

--- app/bots/buyer_bot/handlers/payment.py
+++ app/bots/buyer_bot/handlers/payment.py
@@ -97,7 +97,7 @@ async def cancel_active_handler(call: CallbackQuery, state: FSMContext, user):
 
     pending = await db.payment.get_by_status(user.telegram_id, PaymentStatus.PENDING_LINK)
     if pending:
-        await db.payment.cancel_payment(user.telegram_id)
+        await db.payment.set_status(pending.id, PaymentStatus.CANCELLED)
         for admin_id in settings.telegram.ADMIN_IDS:
             await bots.admin.bot.send_message(
                 chat_id=admin_id,
