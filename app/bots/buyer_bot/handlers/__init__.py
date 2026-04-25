1bb00d066da9a562e8df6f706b123ec1bf37cec9
Lev <me@la-oid.dev>
2026-04-25 23:29:17 +0300

    Update __init__.py

--- app/bots/buyer_bot/handlers/__init__.py
+++ app/bots/buyer_bot/handlers/__init__.py
@@ -1,5 +1,6 @@
 from .start import r as start_router
 from .menu import r as menu_router
+from .profile import r as profile_router
 from .wholesale import r as wholesale_router
 from .payment import r as payment_router
 from .check import r as check_router
@@ -7,6 +8,7 @@ from .check import r as check_router
 def include_routers(dp):
     dp.include_router(start_router)
     dp.include_router(menu_router)
+    dp.include_router(profile_router)
     dp.include_router(wholesale_router)
     dp.include_router(payment_router)
     dp.include_router(check_router)
