5f96423e46297cf65d2c563b7e5ddb8eabfd4a8f
Lev <me@la-oid.dev>
2026-04-28 19:09:35 +0300

    Update __init__.py

--- app/bots/admin_bot/keyboards/inline/__init__.py
+++ app/bots/admin_bot/keyboards/inline/__init__.py
@@ -1,5 +1,6 @@
 from .menu import MenuKeyboards
 from .verify import VerifyKeyboards
 from .order import OrderKeyboards
+from .cashout import CashoutKeyboards
 
-__all__ = ["MenuKeyboards", "VerifyKeyboards", "OrderKeyboards"]
+__all__ = ["MenuKeyboards", "VerifyKeyboards", "OrderKeyboards", "CashoutKeyboards"]
