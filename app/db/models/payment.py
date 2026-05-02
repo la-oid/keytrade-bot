655c6351195bffefcb676f6ce813e6f28d7685db
Lev <me@la-oid.dev>
2026-05-02 15:22:32 +0300

    Update payment.py

--- app/db/models/payment.py
+++ app/db/models/payment.py
@@ -14,7 +14,7 @@ class Payment(Base):
     amount = Column(Integer, nullable=False)                                                        # Количество ключей
     price = Column(Numeric(15, 2), nullable=False)                                                  # Сумма к оплате
     
-    bank = Column(String(50), nullable=False)                                                       # Банк (Сбербанк/Тинькофф)
+    bank = Column(String(50), nullable=True)                                                       # Банк (Сбербанк/Тинькофф)
     payment_link = Column(String(512), nullable=True)                                               # URL страницы оплаты
     pdf_path = Column(String(512), nullable=True)                                                   # Путь к сохранённому PDF
     
