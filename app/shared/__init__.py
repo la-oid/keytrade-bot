1658970c392671820f96d9ccef822cc5d6322dee
Lev <me@la-oid.dev>
2026-05-12 08:41:25 +0300

    Add start.png image for seller section

--- app/shared/__init__.py
+++ app/shared/__init__.py
@@ -1,5 +1,5 @@
-from . import constants, paths
+from . import constants, paths, images
 from .config import settings
 from .utils import db, bots, BotBundle, scheduler
 
-__all__ = ["constants", "paths", "settings", "db", "bots", "BotBundle", "scheduler"]
+__all__ = ["constants", "paths", "images", "settings", "db", "bots", "BotBundle", "scheduler"]
---
+++ app/shared/images.py
@@ -0,0 +1,22 @@
+from app.shared.paths import IMAGES_DIR
+
+
+class BuyerImages:
+    """Пути к картинкам buyer_bot."""
+    START            = IMAGES_DIR / "buyer" / "start.png"
+    ABOUT            = IMAGES_DIR / "buyer" / "about.png"
+    PROFILE          = IMAGES_DIR / "buyer" / "profile.png"
+    MEDIUM_WHOLESALE = IMAGES_DIR / "buyer" / "medium_wholesale.png"
+    LARGE_WHOLESALE  = IMAGES_DIR / "buyer" / "large_wholesale.png"
+    ORDER_PENDING    = IMAGES_DIR / "buyer" / "order_pending.png"
+    ORDER_COMPLETED  = IMAGES_DIR / "buyer" / "order_completed.png"
+
+
+class SellerImages:
+    """Пути к картинкам seller_bot."""
+    START           = IMAGES_DIR / "seller" / "start.png"
+    ABOUT           = IMAGES_DIR / "seller" / "about.png"
+    PROFILE         = IMAGES_DIR / "seller" / "profile.png"
+    ORDERS          = IMAGES_DIR / "seller" / "orders.png"
+    PAYMENT_CREATED = IMAGES_DIR / "seller" / "payment_created.png"
+    KEYS_ACCEPTED   = IMAGES_DIR / "seller" / "keys_accepted.png"
